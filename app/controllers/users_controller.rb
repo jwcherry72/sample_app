671cef47bab1439c5527e9c8570e724b2258370f
Jason W. Cherry <jwcherry72@gmail.com>
2013-07-22 20:44:03 +0900

    Chapter 9 exercises

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
-  before_action :correct_user,   only: [:edit, :update]
-  before_action :admin_user,     only: :destroy
+  before_action :signed_in_user,    only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,      only: [:edit, :update]
+  before_action :admin_user,        only: :destroy
+  before_action :already_signed_in, only: [:new, :create]
   
   def index
     @users = User.paginate(page: params[:page])
@@ -67,5 +68,9 @@ class UsersController < ApplicationController
     
     def admin_user
       redirect_to(root_path) unless current_user.admin?
+    end
+    
+    def already_signed_in
+      redirect_to(root_path) if signed_in?
     end
 end
